853476d5f5861285bee0e1c798853eabe2a03b80
darkyboys <darkyboys726@gmail.com>
2025-06-25 10:11:13 +0000

    Initial Release

--- example/specs.cc
+++ example/specs.cc
@@ -8,8 +8,33 @@
 
 // This is the test file for testing the official library for the HELL6.99MO
 #include "../lib/cxx/Model0/ModelO-HELL6.99.hh"
+// #include <cstddef>
 
 int main (){
-    HELL6_99MO myfile ("stresstest/level1.h669");
+    HELL6_99MO myfile ("stresstest/level3.h699");
     myfile.Parse();
+
+    // for (size_t i = 0;i < myfile.string_keys.size();i++){
+    //     std::cout << "Key: "<<myfile.string_keys[i][0]<<", ";
+    //     std::cout << "Value: "<<myfile.string_keys[i][1]<<"\n";
+    //     std::cout << "Key(raw): [" << myfile.string_keys[i][0] << "] len=" << myfile.string_keys[i][0].length() << "\n";
+    //     std::cout << "Ref(raw): [" << "system.logging.level" << "] len=" << std::string("system.logging.level").length() << "\n";
+
+    //     if (myfile.string_keys[i][0] == "system.logging.level"){
+    //         std::cout << myfile.string_keys[i][1]<<"\n";
+    //     }
+    // }
+    // std::cout << myfile.get("system.logging.level").string_value<<"\n\n";
+    // myfile.set("system.logging.level", "Hello world 2024");
+    // std::cout << myfile.get("system.logging.level").string_value<<"\n\n";
+    // std::cout << myfile.get("database.port").number_value<<"\n\n";
+    // myfile.set("database.port", "Hello world 2024");
+    // std::cout << myfile.get("database.port").number_value<<"\n\n";
+    // std::cout << myfile.get("system.logging.enabled").bool_value<<"\n\n";
+    // myfile.set("system.logging.enabled", "false");
+    // std::cout << myfile.get("system.logging.enabled").bool_value<<"\n\n";
+    // std::cout << myfile.get("def.UNIDEF").unidef_value<<"\n\n";
+    // myfile.set("def.UNIDEF", "Hello world 2024");
+    // std::cout << myfile.get("def.UNIDEF").unidef_value<<"\n\n";
+    std::cout <<myfile.get("database.port").type<<"\n";
 }
